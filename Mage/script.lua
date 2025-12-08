d634d34f96706b4364c5441a1fc61908cfaf1701
Tameno <tamenorias@gmail.com>
2025-12-08 12:03:28 +0100

    make model

---
+++ Mage/script.lua
@@ -0,0 +1,367 @@
+local tick_tween = require("tickTween")
+
+local ITEM_FRONT_OFFSET = 10
+local ITEM_SIDE_OFFSET = 11
+local ITEM_UP_OFFSET = 2
+local ITEM_STIFFNESS = 0.4
+local ITEM_DAMPING = 0.5
+local ITEM_SPEED_HELP = 0.5
+local ITEM_BOB_INTENSITY = 2
+local ITEM_BOB_ROTATION_INTENSITY = 5
+local ITEM_BOB_SPEED = 0.13
+local ITEM_USE_DISTANCE = 4
+local ITEM_USE_HEIGHT_OFFSET = 1
+local VANILLA_WALK_SPEED = 0.21585
+local WALK_ANIM_BASE_SPEED = 2
+
+local item_empty = models.model.ItemEmpty
+local world = models.model.World
+local left_item_part = world.CustomLeftItem
+local right_item_part = world.CustomRightItem
+local left_item_pivot = left_item_part.PivotItemLeft
+local right_item_pivot = right_item_part.PivotItemRight
+local left_item_parent = left_item_pivot.OffsetItemLeft
+local right_item_parent = right_item_pivot.OffsetItemRight
+local root = models.model.root
+local head = root.CustomHead
+
+local always_playing_anims = {
+	"stand",
+	"walk_forward",
+	"walk_forward_left_arm",
+	"walk_forward_right_arm",
+	"walk_back",
+	"walk_back_left_arm",
+	"walk_back_right_arm",
+	"hold_item_left",
+	"hold_item_right",
+}
+
+local walk_anims = {
+	"walk_forward",
+	"walk_forward_left_arm",
+	"walk_forward_right_arm",
+	"walk_back",
+	"walk_back_left_arm",
+	"walk_back_right_arm",
+}
+
+local block_face_normals = {
+	["up"] = vec(0.0, 1.0, 0.0),
+	["down"] = vec(0.0, -1.0, 0.0),
+	["north"] = vec(0.0, 0.0, -1.0),
+	["south"] = vec(0.0, 0.0, 1.0),
+	["east"] = vec(1.0, 0.0, 0.0),
+	["west"] = vec(-1.0, 0.0, 0.0)
+}
+
+local item_id_rotations = {
+	["minecraft:fishing_rod"] = vec(55, 0, 0),
+	["minecraft:carrot_on_a_stick"] = vec(55, 0, 0),
+	["minecraft:trident"] = vec(90, 0, 0),
+}
+
+local item_tag_rotations = {
+	["minecraft:swords"] = vec(80, 0, 0),
+	["minecraft:shovels"] = vec(80, 0, 0),
+}
+
+local left_item_task
+local right_item_task
+
+local current_left_item
+local current_right_item
+
+local left_item_speed = vec(0, 0, 0)
+local right_item_speed = vec(0, 0, 0)
+local left_item_prev_desired_pos
+local right_item_prev_desired_pos
+
+local item_bob_progress = 0
+
+vanilla_model.ALL:setVisible(false)
+
+for i, anim_name in ipairs(always_playing_anims) do
+	local anim = animations.model[anim_name]
+	anim:play()
+	anim:blend(0)
+	local function set_blend(blend)
+		anim:blend(blend)
+	end
+	tick_tween:createValue(anim_name .. "_anim_blend", 0.0, set_blend)
+end
+
+local function degSin(x)
+	return math.sin(math.rad(x))
+end
+
+function degCos(x)
+	return math.cos(math.rad(x))
+end
+
+local function itemEquals(a, b)
+	if a == nil or b == nil then
+		return false
+	end
+	if a.id ~= b.id then
+		return false
+	end
+	return true
+end
+
+local function get_item_desired_pos(left)
+	local rot = player:getRot().y
+	local front = vec(degSin(-rot), 0, degCos(-rot)) * ITEM_FRONT_OFFSET
+	local side = vec(-degCos(-rot), 0, degSin(-rot)) * ITEM_SIDE_OFFSET
+	if left then
+		side = side * -1
+	end
+	local up = vec(0, player:getEyeHeight() * 16 + ITEM_UP_OFFSET, 0)
+	return player:getPos() * 16 + front + side + up
+end
+
+local function get_item_rotation(item_stack)
+	if item_stack:isBlockItem() then
+		return vec(45, 0, 0)
+	end
+	local id = item_stack.id
+	local id_rot = item_id_rotations[id]
+	if id_rot ~= nil then
+		return id_rot
+	end
+	for i, tag in ipairs(item_stack:getTags()) do
+		local tag_rot = item_tag_rotations[tag]
+		if tag_rot ~= nil then
+			return tag_rot
+		end
+	end
+	return vec(0, 0, 0)
+end
+
+local function held_item_changed(item_stack, left)
+	local item_pivot
+	local hold_blend_value_name
+	if left then
+		item_pivot = left_item_pivot
+		hold_blend_value_name = "hold_item_left_anim_blend"
+	else
+		item_pivot = right_item_pivot
+		hold_blend_value_name = "hold_item_right_anim_blend"
+	end
+	item_pivot:setRot(get_item_rotation(item_stack))
+	if item_stack.id == "minecraft:air" then
+		tick_tween:setValue(hold_blend_value_name, 0)
+	else
+		tick_tween:setValue(hold_blend_value_name, 1)
+	end
+	if left then
+		current_left_item = item_stack
+	else
+		current_right_item = item_stack
+	end
+end
+
+local function init_item(left)
+	local desired_pos = get_item_desired_pos(left)
+	if left then
+		left_item_prev_desired_pos = desired_pos
+		left_item_part:setPos(desired_pos)
+		left_item_task = left_item_parent:newItem("left_item")
+		left_item_task:setDisplayMode("THIRD_PERSON_LEFT_HAND")
+	else
+		right_item_prev_desired_pos = desired_pos
+		right_item_part:setPos(desired_pos)
+		right_item_task = right_item_parent:newItem("right_item")
+		right_item_task:setDisplayMode("THIRD_PERSON_RIGHT_HAND")
+	end
+end
+
+local function tick_item(left)
+	local part
+	local speed
+	local prev_desired_pos
+	if left then
+		part = left_item_part
+		speed = left_item_speed
+		prev_desired_pos = left_item_prev_desired_pos
+	else
+		part = right_item_part
+		speed = right_item_speed
+		prev_desired_pos = right_item_prev_desired_pos
+	end
+	local bob_progress = item_bob_progress
+	if left then
+		bob_progress = bob_progress + math.pi / 2
+	end
+	local desired_pos = (
+		get_item_desired_pos(left)
+		+ vec(0, math.sin(bob_progress) * ITEM_BOB_INTENSITY, 0)
+	)
+	local desired_speed = desired_pos - prev_desired_pos
+	local part_pos = tick_tween:getPartPos(part)
+	speed:add((desired_pos - part_pos) * ITEM_STIFFNESS)
+	speed:set(math.lerp(speed, desired_speed * ITEM_SPEED_HELP, ITEM_DAMPING))
+	tick_tween:setPartPos(part, part_pos + speed)
+	tick_tween:setPartRot(part, vec(
+		math.sin(bob_progress + math.pi / 2) * ITEM_BOB_ROTATION_INTENSITY,
+		-player:getRot().y,
+		0
+	))
+	prev_desired_pos:set(desired_pos)
+end
+
+local function set_walk_animation_speed(speed)
+	for i, animation_name in ipairs(walk_anims) do
+		animations.model[animation_name]:setSpeed(speed)
+	end
+end
+
+local function set_walk_blend(blend, forward)
+	local back = 1 - forward
+	tick_tween:setValue("walk_forward_anim_blend", blend * forward)
+	tick_tween:setValue("walk_back_anim_blend", blend * back)
+	tick_tween:setValue("stand_anim_blend", 1 - blend)
+	if current_left_item.id == "minecraft:air" then
+		tick_tween:setValue("walk_forward_left_arm_anim_blend", blend * forward)
+		tick_tween:setValue("walk_back_left_arm_anim_blend", blend * back)
+	else
+		tick_tween:setValue("walk_forward_left_arm_anim_blend", 0)
+	end
+	if current_right_item.id == "minecraft:air" then
+		tick_tween:setValue("walk_forward_right_arm_anim_blend", blend * forward)
+		tick_tween:setValue("walk_back_right_arm_anim_blend", blend * back)
+	else
+		tick_tween:setValue("walk_forward_right_arm_anim_blend", 0)
+	end
+end
+
+local function tick_animations()
+	local velocity = player:getVelocity()
+	local velocity_flat = vec(velocity.x, velocity.z)
+	local speed = velocity_flat:length()
+	local rot = player:getRot().y
+	local front = vec(degSin(-rot), degCos(-rot))
+	local forward
+	if front:dot(velocity_flat:normalized()) < -0.01 then
+		forward = 0
+	else
+		forward = 1
+	end
+	if speed > VANILLA_WALK_SPEED then
+		set_walk_animation_speed(speed * WALK_ANIM_BASE_SPEED / VANILLA_WALK_SPEED)
+		set_walk_blend(1, forward)
+	else
+		set_walk_animation_speed(WALK_ANIM_BASE_SPEED)
+		set_walk_blend(speed / VANILLA_WALK_SPEED, forward)
+	end
+end
+
+function events.entity_init()
+	init_item(true)
+	init_item(false)
+end
+
+function events.tick()
+	tick_tween:startTick()
+	local left_item = player:getHeldItem(not player:isLeftHanded())
+	local right_item = player:getHeldItem(player:isLeftHanded())
+	if not itemEquals(left_item, current_left_item) then
+		held_item_changed(left_item, true)
+	end
+	if not itemEquals(right_item, current_right_item) then
+		held_item_changed(right_item, false)
+	end
+	left_item_task:setItem(left_item)
+	right_item_task:setItem(right_item)
+	item_bob_progress = item_bob_progress + ITEM_BOB_SPEED
+	if item_bob_progress > math.pi * 2 then
+		item_bob_progress = item_bob_progress - (math.pi * 2)
+	end
+	tick_item(true)
+	tick_item(false)
+	tick_animations()
+	tick_tween:endTick()
+end
+
+function events.world_render(delta)
+	tick_tween:world_render(delta)
+end
+
+function events.render(delta)
+	tick_tween:render(delta)
+	head:setRot(vanilla_model.HEAD:getOriginRot())
+end
+
+function events.item_render(item, mode, pos, rot, scale, left)
+	if mode == "FIRST_PERSON_LEFT_HAND" or mode == "FIRST_PERSON_RIGHT_HAND" then
+		return item_empty
+	end
+end
+
+if host:isHost() then
+
+	local item_use_pos
+
+	local function update_item_use_pos()
+		local entity = host:getPickEntity()
+		local block, block_hit_pos, face = host:getPickBlock()
+		if entity ~= nil then
+			local bounding_box = entity:getBoundingBox()
+			local entity_pos = entity:getPos()
+			item_use_pos = (
+				entity_pos
+				+ vec(
+					0,
+					bounding_box.y / 2.0,
+					0
+				)
+			) * 16
+			local pos_offset = player:getPos() - entity_pos
+			pos_offset.y = 0
+			pos_offset:normalize()
+			pos_offset = pos_offset * (
+				bounding_box.x
+				* 8
+				+ ITEM_USE_DISTANCE
+			)
+			item_use_pos.y = item_use_pos.y + ITEM_USE_HEIGHT_OFFSET
+			item_use_pos:add(pos_offset)
+		elseif block.id ~= "minecraft:air" then
+			item_use_pos = block_hit_pos * 16
+		end
+	end
+	local use_key = keybinds:fromVanilla("key.use")
+	local attack_key = keybinds:fromVanilla("key.attack")
+	use_key:setOnPress(update_item_use_pos)
+	attack_key:setOnPress(update_item_use_pos)
+
+	function events.tick()
+		local swing_arm = player:getSwingArm()
+		if swing_arm ~= nil and player:getSwingTime() == 0 then
+			if item_use_pos == nil then
+				update_item_use_pos()
+			end
+			local left = (swing_arm == "MAIN_HAND") == player:isLeftHanded()
+			pings.useItem(left, item_use_pos)
+		end
+		item_use_pos = nil
+	end
+
+end
+
+function pings.useItem(left, pos)
+	if pos == nil then
+		return
+	end
+	local item_part
+	local speed
+	if left then
+		item_part = left_item_part
+		speed = left_item_speed
+	else
+		item_part = right_item_part
+		speed = right_item_speed
+	end
+	tick_tween:setPartPos(item_part, pos)
+	speed:set(0, 0, 0)
+end
---
+++ Mage/tickTween.lua
@@ -0,0 +1,392 @@
+--#region UTILITY
+
+local cos, sin, asin, acos, atan2, abs, sqrt = math.cos, math.sin, math.asin, math.acos, math.atan2, math.abs, math.sqrt
+local pi = math.pi
+
+-- function stolen from https://gist.github.com/hashmal/874792
+local function tprint (tbl, indent)
+  if not indent then indent = 0 end
+  for k, v in pairs(tbl) do
+    formatting = string.rep("  ", indent) ..tostring(k) .. ": "
+    if type(v) == "table" then
+      print(formatting)
+      tprint(v, indent+1)
+    else
+      print(formatting .. tostring(v))
+    end
+  end
+end
+
+-- function stolen from wikipedia
+local function QuatToEuler(q)
+
+	local angles = vec(0, 0, 0)
+
+	-- roll (x-axis rotation)
+	local sinr_cosp = 2 * (q.w * q.x + q.y * q.z)
+	local cosr_cosp = 1 - 2 * (q.x * q.x + q.y * q.y)
+	angles.x = atan2(sinr_cosp, cosr_cosp)
+
+	-- pitch (y-axis rotation)
+	local sinp = sqrt(1 + 2 * (q.w * q.y - q.x * q.z))
+	local cosp = sqrt(1 - 2 * (q.w * q.y - q.x * q.z))
+	angles.y = 2 * atan2(sinp, cosp) - pi / 2
+
+	-- yaw (z-axis rotation)
+	local siny_cosp = 2 * (q.w * q.z + q.x * q.y)
+	local cosy_cosp = 1 - 2 * (q.y * q.y + q.z * q.z)
+	angles.z = atan2(siny_cosp, cosy_cosp)
+
+	return angles:toDeg()
+end
+
+-- function stolen from wikipedia
+local function EulerToQuat(euler)
+
+	euler = euler:toRad()
+
+	-- Abbreviations for the various angular functions
+	local cr = cos(euler.x * 0.5)
+	local sr = sin(euler.x * 0.5)
+	local cp = cos(euler.y * 0.5)
+	local sp = sin(euler.y * 0.5)
+	local cy = cos(euler.z * 0.5)
+	local sy = sin(euler.z * 0.5)
+
+	local q = vec(0, 0, 0, 0);
+	q.w = cr * cp * cy + sr * sp * sy
+	q.x = sr * cp * cy - cr * sp * sy
+	q.y = cr * sp * cy + sr * cp * sy
+	q.z = cr * cp * sy - sr * sp * cy
+
+	return q;
+end
+
+-- function stolen from Godot's source code
+local function QuatDot(a, b)
+	return a.x * b.x + a.y * b.y + a.z * b.z + a.w * b.w;
+end
+
+-- function stolen from Godot's source code
+local function quatSlerp(from, to, weight)
+	local to1 = vec(0, 0, 0, 0);
+	local omega, cosom, sinom, scale0, scale1;
+
+	-- calc cosine
+	cosom = QuatDot(from, to);
+
+	-- adjust signs (if necessary)
+	if (cosom < 0.0) then
+		cosom = -cosom;
+		to1 = -to;
+	else
+		to1 = to;
+	end
+	
+	-- calculate coefficients
+	
+	if ((1.0 - cosom) > 0.00001) then
+		-- standard case (slerp)
+		omega = acos(cosom);
+		sinom = sin(omega);
+		scale0 = sin((1.0 - weight) * omega) / sinom;
+		scale1 = sin(weight * omega) / sinom;
+	else
+		-- "from" and "to" quaternions are very close
+		--  ... so we can do a linear interpolation
+		scale0 = 1.0 - weight;
+		scale1 = weight;
+	end
+	-- calculate final values
+	return vec(
+		scale0 * from.x + scale1 * to1.x,
+		scale0 * from.y + scale1 * to1.y,
+		scale0 * from.z + scale1 * to1.z,
+		scale0 * from.w + scale1 * to1.w
+	);
+end
+
+local function isPartWorldParented(part)
+	while true do
+		if part == nil then
+			return false
+		end
+		if part:getParentType() == "World" then
+			return true
+		end
+		part = part:getParent()
+	end
+end
+
+--#endregion
+
+--#region CLASS DECLARATION
+
+local tickTween = {}
+
+--#endregion
+
+--#region PRIVATE
+
+tickTween.parts = {}
+tickTween.values = {}
+tickTween.worlParts = {}
+tickTween.worldValues = {}
+
+function tickTween:addPartIfNeccesary(part, table)
+	if table[part] == nil then
+		table[part] = {}
+	end
+end
+
+function tickTween:startTickPartsTable(table)
+	for part, transforms in pairs(table) do
+		for transform, data in pairs(transforms) do
+			if transform == "pos" then
+				part:setPos(data.next)
+			end
+			if transform == "quat" then
+				part:setRot(QuatToEuler(data.next))
+			end
+			if transform == "scale" then
+				part:setScale(data.next)
+			end
+			data.prev = data.next
+			data.next = nil
+		end
+	end
+end
+
+function tickTween:startTickValuesTable(table)
+	for value, data in pairs(table) do
+		data.change_func(data.next)
+		data.prev = data.next
+	end
+end
+
+function tickTween:endTickPartsTable(table)
+	for part, transforms in pairs(table) do
+		for transform, data in pairs(transforms) do
+			if data.next == nil then
+				transforms[transform] = nil
+			elseif data.prev == nil then
+				data.prev = data.next
+			end
+		end
+		if transforms.pos == nil and transforms.quat == nil and transforms.scale == nil then
+			table[part] = nil
+		end
+	end
+end
+
+function tickTween:renderPartsTable(table, delta)
+	for part, transforms in pairs(table) do
+		for transform, data in pairs(transforms) do
+			if transform == "pos" then
+				part:setPos(math.lerp(data.prev, data.next, delta))
+			elseif transform == "quat" then
+				part:setRot(QuatToEuler(quatSlerp(data.prev, data.next, delta)))
+			elseif transform == "scale" then
+				part:setScale(math.lerp(data.prev, data.next, delta))
+			end
+		end
+	end
+end
+
+function tickTween:renderValuesTable(table, delta)
+	for value, data in pairs(table) do
+		if data.prev ~= data.next then
+			data.change_func(math.lerp(data.prev, data.next, delta))
+		end
+	end
+end
+
+--#endregion
+
+--#region PUBLIC
+
+---@param name string
+---@param starting_vaule number
+---@param on_change function
+---@param world boolean
+---@return nil
+function tickTween:createValue(name, starting_vaule, on_change, world)
+	world = world or false
+	local table
+	if world then
+		table = self.worldValues
+	else
+		table = self.values
+	end
+	table[name] = {
+		next = starting_vaule,
+		change_func = on_change,
+	}
+	on_change(starting_vaule)
+end
+
+---@return nil
+function tickTween:startTick()
+	self:startTickPartsTable(self.parts)
+	self:startTickPartsTable(self.worlParts)
+	self:startTickValuesTable(self.values)
+	self:startTickValuesTable(self.worldValues)
+end
+
+---@param part ModelPart
+---@param pos Vector3
+---@return nil
+function tickTween:setPartPos(part, pos)
+	local table
+	if isPartWorldParented(part) then
+		table = self.worlParts
+	else
+		table = self.parts
+	end
+    self:addPartIfNeccesary(part, table)
+	local transforms = table[part]
+	if transforms.pos == nil then
+		transforms.pos = {}
+	end
+	transforms.pos.next = pos
+end
+
+---@param part ModelPart
+---@param quat Vector4
+---@return nil
+function tickTween:setPartQuat(part, quat)
+    local table
+	if isPartWorldParented(part) then
+		table = self.worlParts
+	else
+		table = self.parts
+	end
+    self:addPartIfNeccesary(part, table)
+	local transforms = table[part]
+	if transforms.quat == nil then
+		transforms.quat = {}
+	end
+	transforms.quat.next = quat
+end
+
+---@param part ModelPart
+---@param rot Vector3
+---@return nil
+function tickTween:setPartRot(part, rot)
+    tickTween:setPartQuat(part, EulerToQuat(rot))
+end
+
+---@param part ModelPart
+---@param scale Vector3
+---@return nil
+function tickTween:setPartScale(part, scale)
+	local table
+	if isPartWorldParented(part) then
+		table = self.worlParts
+	else
+		table = self.parts
+	end
+    self:addPartIfNeccesary(part, table)
+	local transforms = table[part]
+	if transforms.scale == nil then
+		transforms.scale = {}
+	end
+	transforms.scale.next = scale
+end
+
+---@param part ModelPart
+---@return nil
+function tickTween:getPartPos(part)
+	local transforms = self.parts[part]
+	if transforms == nil then
+		return part:getPos()
+	end
+	local data = transforms.pos
+	if data == nil then
+		return part:getPos()
+	end
+	local next = data.next
+	if next ~= nil then
+		return next
+	end
+	local prev = data.prev
+	if prev ~= nil then
+		return prev
+	end
+	return part:getPos()
+end
+
+---@param part ModelPart
+---@return nil
+function tickTween:getPartRot(part)
+	local transforms = self.parts[part]
+	if transforms == nil then
+		return part:getRot()
+	end
+	local data = transforms.pos
+	if data == nil then
+		return part:getRot()
+	end
+	local next = data.next
+	if next ~= nil then
+		return QuatToEuler(next)
+	end
+	local prev = data.prev
+	if prev ~= nil then
+		return QuatToEuler(prev)
+	end
+	return part:getRot()
+end
+
+---@param part ModelPart
+---@return nil
+function tickTween:getPartScale(part)
+	local transforms = self.parts[part]
+	if transforms == nil then
+		return part:getScale()
+	end
+	local data = transforms.scale
+	if data == nil then
+		return part:getScale()
+	end
+	local next = data.next
+	if next ~= nil then
+		return next
+	end
+	local prev = data.prev
+	if prev ~= nil then
+		return prev
+	end
+	return part:getScale()
+end
+
+---@param name any
+---@param value any
+---@return nil
+function tickTween:setValue(name, value)
+	self.values[name].next = value
+end
+
+---@return nil
+function tickTween:endTick()
+	self:endTickPartsTable(self.parts)
+	self:endTickPartsTable(self.worlParts)
+end
+
+function tickTween:render(delta)
+	self:renderPartsTable(self.parts, delta)
+	self:renderValuesTable(self.values, delta)
+end
+
+function tickTween:world_render(delta)
+	self:renderPartsTable(self.worlParts, delta)
+	self:renderValuesTable(self.worldValues, delta)
+end
+
+--#endregion
+
+--#region OUTPUT
+
+return tickTween
+
+--#endregion
